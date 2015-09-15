e9dc2a5140769889d3fa97c4cbac12341e17caaf
Zax <Zax@zax.com>
2015-09-15 21:10:37 +0200

    Wszystko działa czas na mechanizm logowania

--- Ioserver/ioserver.js
+++ Ioserver/ioserver.js
@@ -1,9 +1,49 @@
+var mqtt = require('mqtt');
 var server = require('http').createServer();
 var io = require('socket.io')(server);
+
+var client = mqtt.connect('mqtt://localhost');
+//var client = mqtt.connect('mqtt://192.168.1.5'); // adres Ip brokera
+
 io.on('connect', function(socket){
   console.log("User connected");
   socket.on('disconnect', function(){
     console.log("User disconnected");
   });
 });
+
+client.on('connect', function () {
+    client.subscribe([
+        "Gripper/Alive",
+        "Arm/Alive",
+        "Chassis/Alive",
+        "Gps/Alive",
+        "GpsUSB/Alive",
+
+        "Gripper/GripperCurrent",
+        "Gripper/MoveCurrent",
+        "Gripper/RotateCurrent",
+        "Gripper/RotatePosition",
+        "Gripper/Voltage",
+
+        "Arm/BarkPosition",
+        "Arm/LokiecPosition",
+        "Arm/ObrotPosition",
+
+        "GpsUSB/LatitudeDD",
+        "GpsUSB/LongitudeDD",
+        "GpsUSB/Track",
+        "GpsUSB/Speed",
+        "COMPAS/DATA",
+
+        "Chassis/AkuMot",
+        "Chassis/AkuEle"
+    ]);
+});
+
+client.on('message', function (topic, payload) {
+  console.log(topic+'='+payload);
+  io.emit('mqtt', {'topic':String(topic), 'payload':String(payload)});
+});
+
 server.listen(3000);
