dbc02496ac803c90aba4fd531e709b230ca13be3
Zax <Zax@zax.com>
2015-11-29 12:18:08 +0100

    Authentication module added

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def main_dashboard(request):
     return render(request, 'Dashboard/main_dashboard.html', {})
